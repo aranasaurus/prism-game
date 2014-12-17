b7d9c11720b2572d69f791423a9dbb5ce739293b
Ryan Arana <ryan@ryanarana.com>
2014-12-17 10:24:45 -0800

    Picked a name

--- conf.lua
+++ conf.lua
@@ -1,12 +1,12 @@
 require( "ext/cupid" )
 
 function love.conf( t )
-    t.identity = "HitBox"
+    t.identity = "Prism"
     t.version = "0.9.1"
     t.author = "@aranasaurus"
 
     t.window.highdpi = false
-    t.window.title = "LightBox"
+    t.window.title = "Prism"
     t.window.width = 1280
     t.window.height = 720
 
